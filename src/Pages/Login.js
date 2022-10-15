d5ba248f9552e938d335969111a7b2b07703eadc
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-10-15 18:18:42 +0530

    Centered the Log In and Sign Up

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,17 @@
 
 const Login = () => {
     return (
-        <div className="container p-3 rounded" style={{ width: "15rem", background: "white" }}>
-            <h1 className="text-center mt-1">Login</h1>
-            <hr></hr>
-            <input type="email" placeholder="Enter Email" className="w-100 mt-3 mb-3 p-1"></input>
-            <input type="password" className="w-100 p-1" placeholder="Enter Password"></input>
-            <input type="checkbox" className="mt-3" id="remember"></input>
-            <label className="text-muted ms-1" for="remember">Remember Me</label>
-            <br></br>
-            <button className="btn btn-outline-success mt-3">Sign In</button>
+        <div className="d-flex align-items-center" style={{ background: "#031222", height: "100vh" }}>
+            <div className="container p-3 rounded" style={{ width: "18rem", background: "white" }}>
+                <h1 className="text-center mt-3 fs-2">Log In</h1>
+                <hr></hr>
+                <input type="email" placeholder="Enter Email" className="w-100 mt-3 mb-3 p-2 "></input>
+                <input type="password" className="w-100 p-2" placeholder="Enter Password"></input>
+                <input type="checkbox" className="mt-3" id="remember"></input>
+                <label className="text-muted ms-1" for="remember">Remember Me</label>
+                <br></br>
+                <button className="btn btn-success mt-3 w-100">Log In</button>
+            </div>
         </div>
     )
 }
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,16 +1,18 @@
 
 export default function SignUp() {
     return (
-        <div className="container p-3 rounded" style={{ width: "15rem", background: "white" }}>
-            <h1 className="text-center mt-1 fs-2">Sign Up</h1>
-            <hr></hr>
-            <input className='mt-1 mb-3 p-1 w-100' type="text" placeholder='First Name' required></input>
-            <input className='p-1 w-100' type="text" placeholder='Last Name' required></input>
-            <input type="email" placeholder="Enter Email" className="w-100 mt-3 mb-3 p-1" required></input>
-            <input type="password" className="w-100 p-1" placeholder="Enter Password" required></input>
-            <input type="password" className="w-100 p-1 mt-3" placeholder="Confirm Password" required></input>
-            <br></br>
-            <button type="submit" className="btn btn-outline-success mt-3">Sign Up</button>
+        <div className="d-flex align-items-center" style={{ height:"100vh", background:"#031222" }}>
+            <div className="container p-3 rounded" style={{ width: "18rem", background: "white"  }}>
+                <h1 className="text-center mt-1 fs-2">Sign Up</h1>
+                <hr></hr>
+                <input className='mt-1 mb-3 p-2 w-100' type="text" placeholder='First Name' required></input>
+                <input className='p-2 w-100' type="text" placeholder='Last Name' required></input>
+                <input type="email" placeholder="Enter Email" className="w-100 mt-3 mb-3 p-2" required></input>
+                <input type="password" className="w-100 p-2" placeholder="Enter Password" required></input>
+                <input type="password" className="w-100 p-2 mt-3" placeholder="Confirm Password" required></input>
+                <br></br>
+                <button type="submit" className="btn btn-success mt-3 w-100">Sign Up</button>
+            </div>
         </div>
     )
 }
