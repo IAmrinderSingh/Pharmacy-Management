a0b0af40008fd9494b4ff88c8a751e9fb1d513ca
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-10-16 12:19:04 +0530

    Importing Pages and Routing them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Router>
+        <Routes>
+          <Route path='login' element={<Login/>}>
+
+          </Route>
+          <Route path='signup' element={<SignUp/>}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
